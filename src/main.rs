2f4bfc229417c1975871b66b0a7756193804cd30
Daniel Pagan <dpagan@ibm.com>
2019-10-30 14:04:38 -0500

    Adding HTTPS support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use actix_files as fs;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[derive(Deserialize)]
 struct FormData {
     fname: String,
     lname: String,
-    street: String,
     city: String,
 }
 
@@ -15,28 +15,31 @@ fn index() -> impl Responder {
 }
 
 fn display_user(form: web::Form<FormData>) -> impl Responder {
-    if form.fname.len() > 30 || form.lname.len() > 30 || form.street.len() > 30 || form.city.len() > 30 {
+    if form.fname.len() > 22 || form.lname.len() > 22 || form.city.len() > 22 {
         return HttpResponse::BadRequest().body("Input too long")
     }
 
     let data = format!(
-        "Hello, {} {}! You live in {} at {}.",
+        "Hello, {} {}! You live in {}.",
         form.fname.to_uppercase(),
         form.lname.to_uppercase(),
         form.city.to_uppercase(),
-        form.street.to_uppercase()
     );
     HttpResponse::Ok().content_type("text/html").body(data)
 }
 
 fn main() {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder.set_private_key_file("key.pem", SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
+
     HttpServer::new(|| {
         App::new()
             .route("/hello", web::get().to(index))
             .route("/show_user", web::post().to(display_user))
             .service(fs::Files::new("/", "./static").index_file("index.html"))
     })
-    .bind("127.0.0.1:8081")
+    .bind_ssl("127.0.0.1:8088", builder)
     .unwrap()
     .run()
     .unwrap();
